4d299ca290ed75d726701be2137ee99a97adcbc6
viacheslav.pivtorak@empeek.io <boojaka@gmail.com>
2019-06-11 14:10:22 +0300

    initial commit

---
+++ src/SteelCalculator/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SteelCalculator extends Component {
+  render() {
+    return (
+      <div>
+        Калькулятор
+      </div>
+    );
+  }
+}
+
+export default SteelCalculator;
